1f547605d2e876a252d9748dce39a9524e47b453
Demian Ivan <hagen_id@ukr.net>
2019-11-21 23:44:48 +0200

    create lof function

--- http_srv.go
+++ http_srv.go
@@ -3,11 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/juju/loggo"
+
 )
 
 func main() {
+	
+	logger := loggo.GetLogger("foo.bar")
+	writer, err := loggo.RemoveWriter("default")
+	// err is non-nil if and only if the name isn't found.
+	loggo.RegisterWriter("default", writer)
+	loggo.ReplaceDefaultWriter(loggocolor.NewWriter(os.Stderr))
+
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
 	http.ListenAndServe(":80", nil)
+
+
+
 }
